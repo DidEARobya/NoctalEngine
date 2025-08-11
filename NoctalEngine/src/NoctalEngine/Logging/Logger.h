c134a29da6577c5939b7716f166b6b2c5005fa61
Ben Higham <94679239+DidEARobya@users.noreply.github.com>
2025-08-11 22:42:45 +0100

    Update Logger.h

--- NoctalEngine/src/NoctalEngine/Logging/Logger.h
+++ NoctalEngine/src/NoctalEngine/Logging/Logger.h
@@ -20,12 +20,12 @@ namespace NoctalEngine
 
 #define NE_ENGINE_TRACE(...) ::NoctalEngine::Logger::GetEngineLogger()->trace(__VA_ARGS__)
 #define NE_ENGINE_INFO(...) ::NoctalEngine::Logger::GetEngineLogger()->info(__VA_ARGS__)
-#define NE_ENGINE_WARN(...) ::NoctalEngine::Logger::GetEngineLogger()->warn("{0} (Line: {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
-#define NE_ENGINE_ERROR(...) ::NoctalEngine::Logger::GetEngineLogger()->error("{0} (Line: {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
-#define NE_ENGINE_FATAL(...) ::NoctalEngine::Logger::GetEngineLogger()->critical("{0} (Line: {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
+#define NE_ENGINE_WARN(...) ::NoctalEngine::Logger::GetEngineLogger()->warn("{0} (Line {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
+#define NE_ENGINE_ERROR(...) ::NoctalEngine::Logger::GetEngineLogger()->error("{0} (Line {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
+#define NE_ENGINE_FATAL(...) ::NoctalEngine::Logger::GetEngineLogger()->critical("{0} (Line {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
 
 #define NE_TRACE(...) ::NoctalEngine::Logger::GetClientLogger()->trace(__VA_ARGS__)
 #define NE_INFO(...) ::NoctalEngine::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define NE_WARN(...) ::NoctalEngine::Logger::GetClientLogger()->warn("{0} (Line: {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
-#define NE_ERROR(...) ::NoctalEngine::Logger::GetClientLogger()->error("{0} (Line: {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
-#define NE_FATAL(...) ::NoctalEngine::Logger::GetClientLogger()->critical("{0} (Line: {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
+#define NE_WARN(...) ::NoctalEngine::Logger::GetClientLogger()->warn("{0} (Line {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
+#define NE_ERROR(...) ::NoctalEngine::Logger::GetClientLogger()->error("{0} (Line {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
+#define NE_FATAL(...) ::NoctalEngine::Logger::GetClientLogger()->critical("{0} (Line {1}): {2}", __FILE__, __LINE__, __VA_ARGS__)
